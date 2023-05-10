030e70be27749976f1562b4d197da01790321a4d
vivek-mahajan-xero <vivek.mahajan@xero.com>
2023-05-11 09:21:21 +1000

    Learn Async

---
+++ Async/Program.cs
@@ -0,0 +1,10 @@
+﻿var getHtmlTask = GetHtmlAsync("https://www.google.com/");
+Console.WriteLine("Waiting for the task to complete...");
+var result = await getHtmlTask;
+Console.WriteLine(result[..10]);
+
+async Task<string> GetHtmlAsync(string url)
+{
+    var httpClient = new HttpClient();
+    return await httpClient.GetStringAsync(url);
+}
