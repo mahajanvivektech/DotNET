29c8353a4006ea682402f3855de628a5df2fe8ef
vivek-mahajan-xero <vivek.mahajan@xero.com>
2023-05-16 09:21:20 +1000

    Type Pattern in pattern matching

--- PatternMatching/Program.cs
+++ PatternMatching/Program.cs
@@ -31,7 +31,7 @@ void NewTypePattern()
     if (o is Hero h)
     {
         Console.WriteLine($"h is of type {h.GetType().Name}");
-        Console.WriteLine($"h is a Hero and is called {h.HeroName}");
+        Console.WriteLine($"o is a Hero and is called {h.HeroName}");
     }
 
     if (o is not Hero) Console.WriteLine("No hero");
@@ -39,7 +39,66 @@ void NewTypePattern()
 
 NewTypePattern();
 
+void TypePatternAndCollections()
+{
+    IEnumerable<Person> persons = new[]
+    {
+        new Person("John", "Doe"),
+        new Hero("Wade", "Wilson", "Deadpool", HeroType.FailedExperiment, false)
+    };
+
+    if (persons is IReadOnlyList<Person> pList && pList[1] is Hero h)
+    {
+        Console.WriteLine($"h is a Hero and is called {h.HeroName}");
+    }
+}
+
+TypePatternAndCollections();
+
+void NullCheckWithTypePattern()
+{
+    Hero? someone = null;
+    Console.WriteLine(someone is Hero { Type: HeroType.FailedExperiment }
+        ? $"someone is {someone.HeroName} and is not null"
+        : "someone is null");
+}
+
+NullCheckWithTypePattern();
+
 #endregion
+
+#region Type pattern in Switch
+
+WriteHeaderLine("Type Patterns in `Switch` statement");
+
+void TypePatternInSwitch()
+{
+    object o = new Hero("Wade", "Wilson", "Deadpool", HeroType.FailedExperiment, false);
+    switch (o)
+    {
+        case "Foo":
+            Console.WriteLine("is a string Foo");
+            break;
+        case string s:
+            Console.WriteLine($"o is a string {s}");
+            break;
+        case Hero h:
+            break;
+        case Person p:
+            Console.WriteLine($"o is a person and has {p.FirstName}");
+            break;
+        case var obj: // var pattern
+            Console.WriteLine($"o is just an object of type {o.GetType().Name}");
+            break;
+        default:
+            throw new InvalidOperationException("Hmm, this should never happen...");
+    }
+}
+
+TypePatternInSwitch();
+
+#endregion
+
 
 void WriteHeaderLine(string pattern) => Console.WriteLine(pattern);
 
@@ -53,4 +112,7 @@ internal enum HeroType
     Other
 };
 
-internal record Hero(string FirstName, string LastName, string HeroName, HeroType Type, bool CanFly);
+internal record Person(string FirstName, string LastName, int? Age = null, Person? Assistant = null);
+
+internal record Hero(string FirstName, string LastName, string HeroName, HeroType Type, bool CanFly) : Person(FirstName,
+    LastName);
